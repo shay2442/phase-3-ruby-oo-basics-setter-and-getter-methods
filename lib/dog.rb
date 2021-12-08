7929503de80988d5f9c06580198149eb6440e050
shaywacker <shay2442@yahoo.com>
2021-12-08 15:25:24 -0600

    commit tree 51b2aa9ec0ea8f15defee7fa07def55eab088ed9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
+require "pry"
 class Dog
+   attr_accessor :name, :breed
 
+    def initialize(name, breed)
+        @name = name 
+        @breed = breed   
+    end
+
+    # def name
+    #     @name
+    # end
 end
+d1 = Dog.new("Roofus", "shitzu")
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
+require "pry"
 class Person
+    attr_accessor :name, :job
+    def initialize(name, job)
+        @name = name
+        @job = job
+    end
 
 end
+
+p1 = Person.new("shay", "student")
